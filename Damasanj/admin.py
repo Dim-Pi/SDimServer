e0f373ecc3417fe3523c5352abb81337bebd0ac9
ali-04 <mahmohamad560@gmail.com>
2021-01-11 08:58:08 +0330

    add class

--- Damasanj/admin.py
+++ Damasanj/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+from Damasanj.models import Lesson , Dor ,Question ,User ,Image ,Role ,door
 
-from Damasanj.models import Lesson , Dor ,Question ,User 
 
 admin.site.register(Lesson)
 admin.site.register(Dor)
 admin.site.register(User)
 admin.site.register(Question)
+admin.site.register(Image)
+admin.site.register(Role)
+admin.site.register(door)
--- Damasanj/models.py
+++ Damasanj/models.py
@@ -12,7 +12,8 @@ protect  =  models.PROTECT
 dtime    =  models.TimeField
 resume   =  models.CASCADE
 dimage   =  models.BinaryField
-from library import FilesConnect as Fldb
+dont     =  models.DO_NOTHING
+from Damasanj.library import FilesConnect as Fldb
 from os  import  mkdir 
 from sys import  argv
 import base64
@@ -35,12 +36,19 @@ def apend(sef,loc,mo):
     return sef
 
 
-class image(model):
+class Image(model):
 
-    name = dstr(primary_key=True)
-    def __init__ (self,loc):
-        self.loc = loc 
+    loc  = dstr(primary_key=True,default='m0') 
 
+
+
+    def insert (self,loc):
+        db = Fldb()
+        cow = db['damaimage']
+        with open(loc ,'rb') as fin :
+            f = Binary(fin.read())
+            cow.insert ({'loc':self.loc,'image':f}) 
+            return self
 
     
     
@@ -56,36 +64,66 @@ class image(model):
             return fileloc
 
 
-def insert (loc,name):
-    db = Fldb()
-    cow = db['damaimage']
-    with open(loc ,'rb') as fin :
-        f = Binary(fin.read())
-        cow.insert ({'loc':name,'image':f}) 
-        return image(name)
 
 
 class Lesson(model):
-    dadmin   =  dclass('user',on_delete=protect)
+
+    dadmin   =  dclass('user',on_delete=dont)
     name     =  dchar(max_length=20,primary_key=True)
-    topics   =  dlist(dstr())
+    topics   =  dlist(dchar(max_length=100))
+
 
     def __str__ (self):
         return self.name
     
 
 
+
+
 class User(model):
-    Sname = dchar(max_length=220)
-    Bname = dchar(max_length=220)
-    stuid = dchar(max_length= 18  ,primary_key=True)
-    Sid   = dchar(max_length=200)
-    model = dchar(max_length=  6  ,choices=[['dmn','dmn'],['Bdmn','Bdmn'],['stu','stu']])
-    mode  = dchar(max_length= 25)
+    Sname           =    dchar (max_length=220)
+    Bname           =    dchar (max_length=220)
+    stuid           =    dchar (max_length= 18  ,primary_key=True)
+    role            =    dclass('Role',on_delete=dont,default=None)
+    Sid             =    dchar (max_length=225 ,default=None)
+    model           =    dchar (max_length=  6 ,default=None)
+    mode            =    dchar (max_length= 25 ,default=None)
+
+    def __str__ (self):
+        return "%s  %s"%(self.Sname,self.Bname)
     
 
-    def __init__ (self,Sid):
-        self.Sid = Sid
+
+
+
+
+class door (model):
+    door       =  dclass('Door',on_delete=resume)
+    body       =  dclass('User',on_delete=resume)
+    Progress   =  dint()
+    def __str__(self):
+        return '%s : %s' %(self.body,self.door)
+
+
+
+
+
+
+
+
+
+
+
+
+class Role(model):
+    name = dstr(primary_key=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
 
 
 
@@ -98,7 +136,7 @@ class Dor(model):
     topic      =   dlist(dstr())
     qtion      =   ddict()
     toool      =   dint()
-    name       =   dchar(max_length=50,primary_key=True)
+    name       =   dchar(max_length=50,primary_key=True,default='name')
 
     def __str__ (self):
         return "%s: ( %s )" %(str(self.lesson)   , ','.join(self.topic)  )
@@ -108,8 +146,8 @@ class Dor(model):
 
 
 class Question(model):
-    name     =   dchar(max_length=60,primary_key=True)
-    image    =   dimage(blank=True)
+    name     =   dchar(max_length=60,primary_key=True,default='Ques')
+    image    =   dclass('Image',on_delete=resume)
     text     =   dstr()
     lesson   =   dclass("Lesson",on_delete=protect)
 
